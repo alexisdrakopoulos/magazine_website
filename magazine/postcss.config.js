8b480560f21ac7625189af080eb30fa634a0f8e5
Alexis Drakopoulos <alexis.drakopoulos95@gmail.com>
2023-07-30 18:09:58 +0200

    update css and title

---
+++ magazine/postcss.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    plugins: [
+    'postcss-obfuscator',
+      process.env.NODE_ENV === 'production'
+        ? [
+            'postcss-flexbugs-fixes',
+            [
+              'postcss-preset-env',
+              {
+                autoprefixer: {
+                  flexbox: 'no-2009',
+                },
+                stage: 3,
+                features: {
+                  'custom-properties': false,
+                },
+              },
+            ],
+          ]
+        : [
+            // No transformations in development
+          ],
+        ]
+  }
