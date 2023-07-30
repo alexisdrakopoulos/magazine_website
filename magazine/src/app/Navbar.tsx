77e2c1c0782a99b6bbeae93d8a635e974bba3f82
Alexis Drakopoulos <alexis.drakopoulos95@gmail.com>
2023-07-30 20:35:47 +0200

    update navbar font and mess with logo settings

--- magazine/src/app/Navbar.tsx
+++ magazine/src/app/Navbar.tsx
@@ -1,37 +1,53 @@
 import Link from "next/link";
 import Image from "next/image";
+import { Trispace } from "next/font/google";
+
+const trispace = Trispace({
+  weight: "500",
+  subsets: ["latin"],
+});
 
 export default function Navbar() {
   return (
     <nav className="PageNav">
       <div className="u-Container">
         <div className="column">
-          <h1 className="logo gap">
-            <a href="/">
-              <Image
-                src="/temporary_logo.svg"
-                alt="minorversion"
-                width={0}
-                height={0}
-                sizes="100vw"
-                style={{ width: "100%", height: "auto" }} // optional
-              ></Image>
-            </a>
-          </h1>
           <ul className="nav">
+            <li className="logo">
+              <Link href="/">
+                <Image
+                  src="/temporary_logo.svg"
+                  alt="minorversion"
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                  style={{
+                    width: "100%",
+                    height: "auto",
+                    paddingBottom: "3px",
+                  }} // optional
+                ></Image>
+              </Link>
+            </li>
             <li>
               <Link href="/topics/">
-                <span>Topics</span>
+                <span className={["nav-item", trispace.className].join(" ")}>
+                  Topics
+                </span>
               </Link>
             </li>
             <li>
               <Link href="/about/">
-                <span>About</span>
+                <span className={["nav-item", trispace.className].join(" ")}>
+                  About
+                </span>
               </Link>
             </li>
             <li>
               <Link href="/contact/">
-                <span>Contact</span>
+                <span className={["nav-item", trispace.className].join(" ")}>
+                  Contact
+                </span>
               </Link>
             </li>
           </ul>
